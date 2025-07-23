c1223cbdde657335b3194a55ce04f16465617c7b
Fırat IŞILDAK <firatisildak1@gmail.com>
2025-07-23 17:07:33 +0300

    productadd page designed

--- Src/Clients/ECommerce.WebUI/obj/Debug/net8.0/ECommerce.WebUI.AssemblyInfo.cs
+++ Src/Clients/ECommerce.WebUI/obj/Debug/net8.0/ECommerce.WebUI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+57961d0213fd7912633edb6b74ef9aefb7afa2c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3f6b664596a9553cf6b9181f9776f4647bb2b4f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ECommerce.WebUI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
